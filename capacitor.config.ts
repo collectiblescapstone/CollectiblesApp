e9d51a6f5c1678e94b4fb291fa11878aa960a078
kennethkvs <kennethkvs060103@gmail.com>
2025-10-13 23:14:49 -0400

    Add nextjs + tailwind template

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.collectibles.app',
+  appName: 'collectibles-app',
+  webDir: 'public'
+};
+
+export default config;

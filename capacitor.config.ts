d2db12d8aae025ea4787b0c13553b218d9aba405
kennethkvs <kennethkvs060103@gmail.com>
2025-10-13 23:17:34 -0400

    Add capacitor init

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,13 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
   appId: 'com.collectibles.app',
-  appName: 'collectibles-app',
-  webDir: 'public'
-};
+  appName: 'collectiblesApp',
+  webDir: 'out',
+  server: {
+    url: `${process.env.LOCAL_NEXT_SERVER_URL}`,
+    cleartext: true,
+  },
+}
 
-export default config;
+export default config

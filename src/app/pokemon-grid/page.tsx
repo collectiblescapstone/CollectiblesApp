3967193a684e57ad12220585c3eb54b7d9fe2d2b
Elite Lu <elitelulww@gmail.com>
2025-10-30 16:16:49 -0400

    first commit

---
+++ src/app/pokemon-grid/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button, ButtonGroup } from '@chakra-ui/react';
+
+export default function PokemonGridPage() {
+  return (
+    <div>
+      <Button>Type of Grid</Button>
+    </div>
+  );
+}

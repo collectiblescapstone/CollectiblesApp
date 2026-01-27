08c3c0d85595c5c96ff6d9b32d2d4075a2093569
taniadasilva17 <46635552+taniadasilva17@users.noreply.github.com>
2026-01-27 15:16:08 -0500

    JIRA-58: add pages home page

--- src/app/home-page/page.tsx
+++ src/app/home-page/page.tsx
@@ -1,7 +1,59 @@
 'use client';
 
-const HomePage = () => {
-  return <div>Home Page Content</div>;
+import React from 'react';
+
+import PopularCards from "@/components/home/PopularCards";
+import Collection from "@/components/home/Collection";
+import TradeSuggestions from '@/components/home/TradeSuggestions';
+
+import { Box, Flex, Heading, Text } from '@chakra-ui/react';
+
+const HomePage: React.FC = () => {
+
+    return (
+        <Box bg="white" minH="100vh" color="black" mb={4}>
+            <Flex flexDirection="column" alignItems="center" gap={2}>
+                <Flex
+                    flexDirection="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    gap={1}
+                >
+                </Flex>
+            </Flex>
+            <Heading mt={1} fontSize="2xl" textAlign="center" fontWeight={'Bold'} fontFamily="var(--font-sans)">
+                Welcome back [USERNAME]!
+            </Heading>
+
+            <Flex
+                mt={4}
+                px={4}
+                w="100%"
+                justifyContent="space-around"
+                flexWrap="wrap"
+                gap={6}
+            >
+                <Flex direction="column" alignItems="center" minW="180px">
+                    <Text fontSize="2xl" fontWeight="bold">0</Text>
+                    <Text fontSize="sm" color="gray.600">cards logged this month</Text>
+                </Flex>
+
+                <Flex direction="column" alignItems="center" minW="180px">
+                    <Text fontSize="2xl" fontWeight="bold">0</Text>
+                    <Text fontSize="sm" color="gray.600">total cards in collection</Text>
+                </Flex>
+
+                <Flex direction="column" alignItems="center" minW="180px">
+                    <Text fontSize="2xl" fontWeight="bold">0</Text>
+                    <Text fontSize="sm" color="gray.600">cards up for trade</Text>
+                </Flex>
+            </Flex>
+
+            <PopularCards />
+            <Collection />
+            <TradeSuggestions/>
+        </Box>
+    );
 };
 
 export default HomePage;
---
+++ src/components/home/TradeSuggestions.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import React from 'react';
+import Divider from '@/components/user-profile/Divider';
+import { Flex, Image, Text } from '@chakra-ui/react';
+import { useRandomCards } from '@/components/personal-profile/RandomCard'; // for now, change later
+import { PokemonCardImage } from '@/types/personal-profile';
+
+const TradeSuggestions: React.FC = () => {
+    const { cards, loading } = useRandomCards('ex5', 3);
+
+    if (loading) return <Text>Loading cards...</Text>;
+    if (cards.length === 0) return null;
+
+    return (
+        <Flex
+            flexDirection="column"
+            gap={2}
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            w="100%"
+            px={4}
+        >
+            <Divider />
+            <Flex mt={1}>
+                <Text fontSize="md" color="gray.700" fontWeight="semibold" mb={2}>
+                    TradePost Suggestion
+                </Text>
+            </Flex>
+            <Flex
+                flexDirection="row"
+                justifyContent="center"
+                alignItems="center"
+                wrap="wrap"
+                gap={5}
+            >
+                {cards.map((card: PokemonCardImage, index: number) => (
+                    <Flex key={index}>
+                        <Image
+                            src={`${card.image}/high.png`}
+                            alt={card.name}
+                            w="105px"
+                            h="auto"
+                            borderRadius="none"
+                        />
+                    </Flex>
+                ))}
+                <text>[USERNAME]</text>
+            </Flex>
+        </Flex>
+    );
+};
+
+export default TradeSuggestions;

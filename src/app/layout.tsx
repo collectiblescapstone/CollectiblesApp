de14ade9ad6c6c6aa0792d817b8fe4710c7a0c7f
kennethkvs <kennethkvs060103@gmail.com>
2026-01-29 01:19:08 -0500

    Add suspense to enable useSearchParams()

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { ChakraUIProvider } from '@/context/ChakraUIProvider';
 import { AuthContextProvider } from '@/context/AuthProvider';
 import { HeaderProvider } from '@/context/HeaderProvider';
 import Content from './content';
+import { Suspense } from 'react';
+import { Spinner } from '@chakra-ui/react';
 
 export const metadata: Metadata = {
   title: 'Collectibles App',
@@ -21,7 +23,9 @@ export default function RootLayout({
         <HeaderProvider>
           <ChakraUIProvider>
             <AuthContextProvider>
-              <Content>{children}</Content>
+              <Suspense fallback={<Spinner size="xl" />}>
+                <Content>{children}</Content>
+              </Suspense>
             </AuthContextProvider>
           </ChakraUIProvider>
         </HeaderProvider>

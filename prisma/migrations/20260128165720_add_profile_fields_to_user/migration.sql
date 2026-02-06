ffb0831a878283f7e8c7fc374290c55b37bd5617
kennethkvs <kennethkvs060103@gmail.com>
2026-02-06 15:14:58 -0500

    remove items conflicting with main

--- prisma/migrations/20260128165720_add_profile_fields_to_user/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "bio" TEXT,
-ADD COLUMN     "facebook" TEXT,
-ADD COLUMN     "instagram" TEXT,
-ADD COLUMN     "location" TEXT,
-ADD COLUMN     "twitter" TEXT,
-ADD COLUMN     "visibility" TEXT DEFAULT 'Public';
--- supabase/snippets/authToUsers.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO public."User" (id, email)
-SELECT 
-  id, 
-  email 
-FROM auth.users
-ON CONFLICT (id) DO NOTHING;

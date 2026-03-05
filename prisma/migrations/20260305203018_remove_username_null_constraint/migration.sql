4b86f6eeaef70486cc7adb76668de483043e7b9c
kennethkvs <kennethkvs060103@gmail.com>
2026-03-05 18:12:28 -0500

    Fix SQL migration script

--- prisma/migrations/20260305203018_remove_username_null_constraint/migration.sql
+++ prisma/migrations/20260305203018_remove_username_null_constraint/migration.sql
@@ -5,6 +5,9 @@
 
 */
 -- Ensure that username is not null before applying the NOT NULL constraint
+UPDATE public."User"
+SET username = 'user_' || right("id"::text, 8)
+WHERE username IS NULL;
 
 -- AlterTable
 ALTER TABLE "User" ALTER COLUMN "username" SET NOT NULL;

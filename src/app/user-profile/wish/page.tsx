9df4c6a8f795729d5609364a146548c45646953e
kennethkvs <kennethkvs060103@gmail.com>
2026-03-23 18:35:24 -0400

    Refactor profile wishlist page

--- src/app/user-profile/wish/page.tsx
+++ src/app/user-profile/wish/page.tsx
@@ -2,30 +2,20 @@
 
 import React, { useEffect, useState } from 'react'
 
-import {
-    Box,
-    Flex,
-    Heading,
-    Text,
-    Image,
-    Spinner,
-    SimpleGrid
-} from '@chakra-ui/react'
-import { Avatar } from '@chakra-ui/react'
-import { PokemonCardImage } from '@/types/personal-profile'
-import { useSearchParams } from 'next/navigation'
+import { Flex, Text, Spinner } from '@chakra-ui/react'
 import { UserProfile } from '@/types/personal-profile'
 import { fetchUserProfile } from '@/utils/profiles/userNameProfilePuller'
 import { useHeader } from '@/context/HeaderProvider'
-import { profilePictures } from '@/app/personal-profile/edit-profile/constants'
+import ProfileWishlistLayout from '@/components/profiles/ProfileWishlistLayout'
+import { useProfileSelected } from '@/context/ProfileSelectionProvider'
 
 const WishScreen: React.FC = () => {
-    const searchParams = useSearchParams()
     const [user, setUser] = useState<UserProfile | null>(null)
     const [loading, setLoading] = useState<boolean>(true)
     const [error, setError] = useState<string | null>(null)
-    const userName = searchParams.get('username')
+
     const { setProfileID } = useHeader()
+    const { profileSelected: userName } = useProfileSelected()
 
     useEffect(() => {
         if (!userName) {
@@ -51,12 +41,6 @@ const WishScreen: React.FC = () => {
 
         loadUserProfile()
     }, [userName, setProfileID])
-
-    const cards =
-        user?.wishlist.map((item) => ({
-            name: item.card.name,
-            image: item.card.image_url
-        })) ?? []
 
     if (loading) {
         return (
@@ -88,72 +72,7 @@ const WishScreen: React.FC = () => {
         )
     }
 
-    return (
-        <Box bg="white" minH="100vh" color="black">
-            <Flex flexDirection="column" gap={6} mt={4}>
-                <Flex
-                    flexDirection="row"
-                    justifyContent="flex-start"
-                    alignItems="center"
-                    gap={4}
-                >
-                    <Avatar.Root boxSize="100px" p={2} background="white">
-                        <Avatar.Image
-                            objectFit="contain"
-                            src={
-                                profilePictures[user.profile_pic]?.path ??
-                                profilePictures[0].path
-                            }
-                        />
-                    </Avatar.Root>
-                    <Flex
-                        flexDirection="column"
-                        justifyContent="flex-start"
-                        alignItems="flex-start"
-                        gap={2}
-                    >
-                        {user.firstName || user.lastName ? (
-                            <Heading mt={3} fontSize="2xl" fontWeight={'Bold'}>
-                                {user?.firstName} {user?.lastName}
-                            </Heading>
-                        ) : (
-                            <Heading mt={3} fontSize="2xl" fontWeight={'Bold'}>
-                                {user?.username}
-                            </Heading>
-                        )}
-                        <Text
-                            fontSize="md"
-                            color="gray.600"
-                            fontWeight={'semibold'}
-                        >
-                            Wish List - {cards.length} Items
-                        </Text>
-                    </Flex>
-                </Flex>
-                <Flex justifyContent="center" alignItems="center" mt={-4}>
-                    <Box height="3px" width="97%" bg="black" mt={5} />
-                </Flex>
-                <SimpleGrid
-                    columns={{ base: 3 }}
-                    w="100%"
-                    gap={{ base: 10, lg: 14 }}
-                    px={3}
-                >
-                    {cards.map((card: PokemonCardImage, index: number) => (
-                        <Flex key={index}>
-                            <Image
-                                src={`${card.image}`}
-                                alt={card.name}
-                                w="100%"
-                                h="auto"
-                                borderRadius="none"
-                            />
-                        </Flex>
-                    ))}
-                </SimpleGrid>
-            </Flex>
-        </Box>
-    )
+    return <ProfileWishlistLayout user={user} />
 }
 
 export default WishScreen
